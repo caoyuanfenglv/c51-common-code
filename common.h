9a8f3ddfbe3c14237b6b1585d4fa5cd5ef724e2c
Qt Shatp <qtsharp@qq.com>
2011-11-19 04:46:20 +0800

    cool

---
+++ common.h
@@ -0,0 +1,25 @@
+/*--------------------------------------------------------------------------
+COMMON.H
+
+Common header file for C51 and C52 working at 12MHz
+
+Code by QtSharp
+--------------------------------------------------------------------------*/
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+
+/*******************************/
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef unsigned long ulong;
+
+/*******************************/
+
+extern void delay(unsigned char);
+extern void delay_ms(unsigned char);
+
+/*******************************/
+
+#endif
